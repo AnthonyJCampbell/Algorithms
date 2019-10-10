b024b983a0c0c3bce500fbfcf06d03e693827028
AnthonyJCampbell <anthony@anthonyjcampbell.com>
2019-10-10 16:37:48 +0200

    Using answer sheets for making_change

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -11,42 +11,26 @@ def making_change(amount, denominations):
         # Dime = 10
         # Quarter = 25
         # Half dollar = 50
-    # Assume up to n = 300 in the beginning
 
-    # where we'd want the recursion to stop executing. 
-    # What should happen when the amount of cents given is 0?
-    # What should happen when the amount of cents given is negative?
-        # This implies that we're in a second recursion, having just subtracted some value from it
+    # If amount is zero, there's only one way to change that.
+    if amount == 0:
+        print(f"Amount is 0, returning 1")
+        return 1
+
+    # If amount is less than zero, there's an error. Return 0
     if amount < 0:
         return 0
 
-    # If amount is zero, there's only one way to change that.
-    # If amount is less than two (ergo ~1 cent), there's only one way to change that.
-    if amount >= 0 and amount < 2:
-        print(f"Amount is {amount}, adding 1!")
-        return 1
 
-    if amount >= 5 and amount < 10:
-        print(f"Amount is {amount}, adding 2!")
-        return 2
-
-    if amount >= 10 and amount < 15:
-        print(f"Amount is {amount}, adding 4!")
-        return 4
-
-    if amount == 20:
-        return 9
-
-    
+    if len(denominations) <= 0 and amount > 0:
+        return 0
 
     else:
-    # It returns an int that denotes the number of different ways you can make change
-    # This is compiled of the different options of starting with 1, 5, 10, 25 or 50.
-        return making_change(amount - 1, denominations) + making_change(amount - 5, denominations) \
-            + making_change(amount - 10, denominations) + making_change(amount - 25, denominations) \
-            + making_change(amount - 50, denominations)
-            
+        return making_change(amount - denominations[-1], denominations) \
+            + making_change(amount, denominations[:-1])
 
+arr = [1,2,3]
+print(arr[-1])
 
 if __name__ == "__main__":
 # Test our your implementation from the command line
